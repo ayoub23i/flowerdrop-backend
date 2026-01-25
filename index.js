fd7c4ae1900c0819a414c8732a8d18d1ec9f689a
ayoub23i <65514289+ayoub23i@users.noreply.github.com>
2026-01-25 14:55:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("OK");
+  res.status(200).send("OK");
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Listening on port", PORT);
 });
